39b309d7e24f7b0e73aee988c90c9a42b0e66e7a
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2022-11-16 08:30:42 +0100

    feat: remove subscription audit logs

--- main.tf
+++ main.tf
@@ -1,5 +1,3 @@
-data "azurerm_subscription" "current" {}
-
 resource "azurerm_log_analytics_workspace" "this" {
   name                = var.workspace_name
   resource_group_name = var.resource_group_name
@@ -33,55 +31,5 @@ resource "azurerm_monitor_diagnostic_setting" "this" {
       days    = 0
       enabled = false
     }
-  }
-}
-
-resource "random_id" "this" {
-  byte_length = 8
-}
-
-resource "azurerm_monitor_diagnostic_setting" "subscription" {
-  name                       = "audit-logs-${random_id.this.hex}"
-  target_resource_id         = data.azurerm_subscription.current.id
-  log_analytics_workspace_id = azurerm_log_analytics_workspace.this.id
-
-  log {
-    category = "Administrative"
-    enabled  = true
-  }
-
-  log {
-    category = "ServiceHealth"
-    enabled  = false
-  }
-
-  log {
-    category = "ResourceHealth"
-    enabled  = false
-  }
-
-  log {
-    category = "Alert"
-    enabled  = false
-  }
-
-  log {
-    category = "Autoscale"
-    enabled  = true
-  }
-
-  log {
-    category = "Recommendation"
-    enabled  = false
-  }
-
-  log {
-    category = "Security"
-    enabled  = true
-  }
-
-  log {
-    category = "Policy"
-    enabled  = true
   }
 }
