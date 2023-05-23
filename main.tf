2cb87b49a04044ed3faefef487c9862e094ee382
Kristian Vallejos <126077239+kov117@users.noreply.github.com>
2023-05-23 10:57:16 +0200

    feat!: disable local authentication by default (#26)
    * feat!: disable local authentication by default
    
    * feat!: set local authentication disabled to true

--- main.tf
+++ main.tf
@@ -1,9 +1,10 @@
 resource "azurerm_log_analytics_workspace" "this" {
-  name                = var.workspace_name
-  resource_group_name = var.resource_group_name
-  location            = var.location
-  sku                 = "PerGB2018"
-  retention_in_days   = var.retention_in_days
+  name                          = var.workspace_name
+  resource_group_name           = var.resource_group_name
+  location                      = var.location
+  local_authentication_disabled = var.local_authentication_disabled
+  sku                           = "PerGB2018"
+  retention_in_days             = var.retention_in_days
 
   tags = var.tags
 }
