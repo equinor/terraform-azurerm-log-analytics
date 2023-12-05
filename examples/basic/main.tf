5a282187943a69452a4f18792b3a79870516ad5b
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2023-12-05 13:30:37 +0100

    docs: development and testing

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -6,16 +6,11 @@ resource "random_id" "this" {
   byte_length = 8
 }
 
-resource "azurerm_resource_group" "this" {
-  name     = "rg-${random_id.this.hex}"
-  location = var.location
-}
-
 module "log_analytics" {
   # source = "github.com/equinor/terraform-azurerm-log-analytics"
   source = "../.."
 
   workspace_name      = "log-${random_id.this.hex}"
-  resource_group_name = azurerm_resource_group.this.name
-  location            = azurerm_resource_group.this.location
+  resource_group_name = var.resource_group_name
+  location            = var.location
 }
